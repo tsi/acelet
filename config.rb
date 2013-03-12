71cfb140eff3edcb00765a35d15c4ccf404c0b37
Tsachi Shlidor <tsi.shlidor@gmail.com>
2013-03-13 01:25:20 +0200

    GH v2

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require "salsa";
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,7 +9,7 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+output_style = :extended
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
--- js/main.js
+++ js/main.js
@@ -1,34 +1,55 @@
 (function($) {
 
   $(document).ready(function() {
-    var acelet, val, opt;
-    var options = {
-      syntax: "text",
-      fontSize: "16px",
-      theme: "ace/theme/monokai",
-      softTabs: true,
-      tabSize: 2,
-      wrapText: true,
-      indentGuides: true,
-      showInvisibles: true
-    };
-    var updateAcelet = function() {
-      acelet = "javascript:(function(){var aceletTab=window.open('about:blank');var script=document.createElement('script');script.type='text/javascript';script.id='aceletScript';script.async=true;script.onload=function(){aceletTab.location=acelet('"+options.syntax+"','"+options.fontSize+"','"+options.theme+"','"+options.softTabs+"','"+options.tabSize+"','"+options.wrapText+"','"+options.indentGuides+"','"+options.showInvisibles+"');aceletTab.focus();};script.src='https://raw.github.com/tsi/acelet/master/acelet.min.js';document.body.appendChild(script);}());";
-      $('a.acelet').attr('href', acelet);
-    };
 
-    $('select, input', 'section.defaults').change(function() {
-      var el = $(this);
-      opt = el.attr('id');
-      val = el.val();
-      if (el.is('[type="checkbox"]')) {
-        val = el.is(':checked') ? true : false;
-      }
-      options[opt] = val;
-      updateAcelet();
+    // Load external contents
+    $("[data-html]").each(function() {
+      el = $(this);
+      var src = $(this).attr("data-html") + ".html";
+      el.load(src, function() {
+        aceletDemo();
+      });
     });
 
-    updateAcelet();
+    // Make the bookmarklet work
+    var aceletDemo = function() {
+      if ($('a.acelet').is('.acelet-processed')) {
+        return false;
+      }
+      $('a.acelet').addClass('acelet-processed');
+      var acelet, val, opt;
+      var options = {
+        syntax: "javascript",
+        fontSize: "16px",
+        theme: "ace/theme/monokai",
+        softTabs: true,
+        tabSize: 2,
+        wrapText: true,
+        indentGuides: true,
+        showInvisibles: true
+      };
+      var updateAcelet = function() {
+        acelet = "javascript:(function(){var aceletTab=window.open('about:blank');var script=document.createElement('script');script.type='text/javascript';script.id='aceletScript';script.async=true;script.onload=function(){aceletTab.location=acelet('"+options.syntax+"','"+options.fontSize+"','"+options.theme+"','"+options.softTabs+"','"+options.tabSize+"','"+options.wrapText+"','"+options.indentGuides+"','"+options.showInvisibles+"');aceletTab.focus();};script.src='https://raw.github.com/tsi/acelet/master/acelet.min.js';document.body.appendChild(script);}());";
+        $('a.acelet').attr('href', acelet).addClass("highlight");
+        window.setTimeout(function() {
+          $('a.acelet').removeClass("highlight");
+        }, 600);
+      };
+
+      $(document).on('change', 'select, input', function() {
+        var el = $(this);
+        opt = el.attr('id');
+        val = el.val();
+        if (el.is('[type="checkbox"]')) {
+          val = el.is(':checked') ? true : false;
+        }
+        options[opt] = val;
+        updateAcelet();
+      });
+
+      updateAcelet();
+      
+    };
 
   });
 })(jQuery);
