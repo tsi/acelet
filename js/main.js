b458edbeb17b3735961a32a3816e6c3de94926f6
Tsachi Shlidor <tsi.shlidor@gmail.com>
2013-03-15 00:06:17 +0200

    Analytics

--- js/main.js
+++ js/main.js
@@ -46,10 +46,40 @@
         options[opt] = val;
         updateAcelet();
       });
-
       updateAcelet();
-      
+
+      // Analytics
+      var trackGA = function() {
+        var isDown = false;
+
+        $('a.acelet').on('mousedown', function(){
+          isDown = true;
+        });
+        $('a.acelet').on('mouseup', function(){
+          _gaq.push(['_trackEvent', $('select#syntax').val(), 'demo', $('select#theme').val()]);
+          isDown = false;
+        });
+        $('#bookmarklet').on('mouseleave', function(){
+          if(isDown){
+            _gaq.push(['_trackEvent', $('select#syntax').val(), 'bookmarked', $('select#theme').val()]);
+            isDown = false;
+          }
+        });
+
+      };
+      trackGA();
+
     };
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-39309928-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 
   });
 })(jQuery);
